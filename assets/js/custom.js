897edb433efcff7bf404d61a7adca05148f8e9a3
9979Gyu <yuqin1161@gmail.com>
2025-01-19 00:35:01 +0800

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,82 +33,42 @@ $(document).ready(function(){
 	
 	// 2. Smooth Scroll spy
 		
-		$('.header-area').sticky({
-           topSpacing:0
-        });
-		
-		//=============
+	$('.header-area').sticky({
+		topSpacing:0
+	});
+	
+	//=============
 
-		$('li.smooth-menu a').bind("click", function(event) {
-			event.preventDefault();
-			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top - 0
-			}, 1200,'easeInOutExpo');
-		});
-		
-		$('body').scrollspy({
-			target:'.navbar-collapse',
-			offset:0
-		});
+	$('li.smooth-menu a').bind("click", function(event) {
+		event.preventDefault();
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top - 0
+		}, 1200,'easeInOutExpo');
+	});
+	
+	$('body').scrollspy({
+		target:'.navbar-collapse',
+		offset:0
+	});
 
 	// 3. Progress-bar
 	
-		var dataToggleTooTip = $('[data-toggle="tooltip"]');
-		var progressBar = $(".progress-bar");
-		if (progressBar.length) {
-			progressBar.appear(function () {
-				dataToggleTooTip.tooltip({
-					trigger: 'manual'
-				}).tooltip('show');
-				progressBar.each(function () {
-					var each_bar_width = $(this).attr('aria-valuenow');
-					$(this).width(each_bar_width + '%');
-				});
+	var dataToggleTooTip = $('[data-toggle="tooltip"]');
+	var progressBar = $(".progress-bar");
+	if (progressBar.length) {
+		progressBar.appear(function () {
+			dataToggleTooTip.tooltip({
+				trigger: 'manual'
+			}).tooltip('show');
+			progressBar.each(function () {
+				var each_bar_width = $(this).attr('aria-valuenow');
+				$(this).width(each_bar_width + '%');
 			});
-		}
-	
-	// 4. owl carousel
-	
-		// i. client (carousel)
-		
-			$('#client').owlCarousel({
-				items:7,
-				loop:true,
-				smartSpeed: 1000,
-				autoplay:true,
-				dots:false,
-				autoplayHoverPause:true,
-				responsive:{
-						0:{
-							items:2
-						},
-						415:{
-							items:2
-						},
-						600:{
-							items:4
+		});
+	}
 
-						},
-						1199:{
-							items:4
-						},
-						1200:{
-							items:7
-						}
-					}
-				});
-				
-				
-				$('.play').on('click',function(){
-					owl.trigger('play.owl.autoplay',[1000])
-				})
-				$('.stop').on('click',function(){
-					owl.trigger('stop.owl.autoplay')
-				})
-
-
-    // 5. welcome animation support
+    // 4. welcome animation support
 
         $(window).load(function(){
         	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
